c1b1b204da9b39e226d7f910a4c36e1cea885fe1
Ruixin Zhang <rz95@cs.duke.edu>
2024-05-09 23:15:17 -0400

    Added python script for checking the total number of images

---
+++ controlnet/script.py
@@ -0,0 +1,9 @@
+import os
+
+total = 0
+for file in os.listdir("/usr/project/xtmp/rz95/Telepresence/raw_images"): #<YOUR_OWN_PATH>
+    underscore = file.rindex("_")
+    total += int(file[underscore+1:])
+print("There are {} images total.".format(total))
+    
+    
