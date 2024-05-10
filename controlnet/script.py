c297021fc5070b593d8c50b45efafbb8f8df5b87
Ruixin Zhang <rz95@cs.duke.edu>
2024-05-10 00:36:36 -0400

    Added src/ from MODNet, successfully did alpha matting

--- controlnet/script.py
+++ controlnet/script.py
@@ -1,7 +1,7 @@
 import os
 
 total = 0
-for file in os.listdir("/usr/project/xtmp/rz95/Telepresence/raw_images"): #<YOUR_OWN_PATH>
+for file in os.listdir("/usr/project/xtmp/rz95/Telepresence/controlnet/raw_images"): #<YOUR_OWN_PATH>
     underscore = file.rindex("_")
     total += int(file[underscore+1:])
 print("There are {} images total.".format(total)) #3522
