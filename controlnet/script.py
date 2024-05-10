e5f104ace262f1afc379f1052bcea1d3ca124002
Ruixin Zhang <rz95@cs.duke.edu>
2024-05-09 23:43:51 -0400

    Added MODNet code for image matting

--- controlnet/script.py
+++ controlnet/script.py
@@ -4,6 +4,8 @@ total = 0
 for file in os.listdir("/usr/project/xtmp/rz95/Telepresence/raw_images"): #<YOUR_OWN_PATH>
     underscore = file.rindex("_")
     total += int(file[underscore+1:])
-print("There are {} images total.".format(total))
+print("There are {} images total.".format(total)) #3522
+
+
     
     
