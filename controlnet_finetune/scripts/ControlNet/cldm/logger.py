5c9a1835cfdaab814e624ef01121823cd277241c
Ruixin Zhang <ruixin.zhang@duke.edu>
2024-07-06 10:19:41 -0400

    Added more debugging related to training

--- controlnet_finetune/scripts/ControlNet/cldm/logger.py
+++ controlnet_finetune/scripts/ControlNet/cldm/logger.py
@@ -5,8 +5,7 @@ import torch
 import torchvision
 from PIL import Image
 from pytorch_lightning.callbacks import Callback
-from pytorch_lightning.utilities.distributed import rank_zero_only
-
+from pytorch_lightning.utilities.rank_zero import rank_zero_only
 
 class ImageLogger(Callback):
     def __init__(self, batch_frequency=2000, max_images=4, clamp=True, increase_log_steps=True,
@@ -71,6 +70,6 @@ class ImageLogger(Callback):
     def check_frequency(self, check_idx):
         return check_idx % self.batch_freq == 0
 
-    def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
+    def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
         if not self.disabled:
             self.log_img(pl_module, batch, batch_idx, split="train")
--- controlnet_finetune/scripts/ControlNet/tutorial_train_sd21.py
+++ controlnet_finetune/scripts/ControlNet/tutorial_train_sd21.py
@@ -8,7 +8,7 @@ from cldm.model import create_model, load_state_dict
 
 
 # Configs
-resume_path = './models/control_sd21_ini.ckpt'
+resume_path = '/usr/project/xtmp/rz95/Telepresence/controlnet_finetune/scripts/ControlNet/models/control_sd21_ini.ckpt'
 batch_size = 4
 logger_freq = 300
 learning_rate = 1e-5
@@ -17,7 +17,7 @@ only_mid_control = False
 
 
 # First use cpu to load models. Pytorch Lightning will automatically move it to GPUs.
-model = create_model('./models/cldm_v21.yaml').cpu()
+model = create_model('./models/cldm_v21.yaml').cpu() 
 model.load_state_dict(load_state_dict(resume_path, location='cpu'))
 model.learning_rate = learning_rate
 model.sd_locked = sd_locked
@@ -28,7 +28,7 @@ model.only_mid_control = only_mid_control
 dataset = MyDataset()
 dataloader = DataLoader(dataset, num_workers=0, batch_size=batch_size, shuffle=True)
 logger = ImageLogger(batch_frequency=logger_freq)
-trainer = pl.Trainer(gpus=1, precision=32, callbacks=[logger])
+trainer = pl.Trainer(accelerator='gpu', devices=1, precision=32, callbacks=[logger])
 
 
 # Train!
